aede3e8257e3f0a335b746c7ef8d81f6f6d0309b
Edgar <Edgar@edgar>
2015-10-25 22:12:09 -0200

    Window icon and title.

--- FXplayer/src/com/FXplayer/Main.java
+++ FXplayer/src/com/FXplayer/Main.java
@@ -1,35 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com;
+package com.FXplayer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Edgar
- */
-public class FXplayer extends Application {
+public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
-        Scene scene = new Scene(root);
-        
+        Scene scene = new Scene(root);       
         stage.setScene(scene);
+        stage.getIcons().add(new Image("icon.png"));
+        stage.setTitle("FXplayer");
         stage.show();
     }
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
     }
