3772925495aedb7bf2a57a9e40c68853cbbd6218
Edgar <Edgar@edgar>
2015-10-26 13:16:16 -0200

    Mouse Change Icon Effect Added

--- FXplayer/src/com/FXplayer/FXMLDocumentController.java
+++ FXplayer/src/com/FXplayer/FXMLDocumentController.java
@@ -7,12 +7,14 @@ import it.sauronsoftware.jave.EncoderException;
 import it.sauronsoftware.jave.EncoderProgressListener;
 import it.sauronsoftware.jave.EncodingAttributes;
 import it.sauronsoftware.jave.MultimediaInfo;
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -36,8 +38,8 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     public void openFile() throws IllegalArgumentException, EncoderException {
-        FileChooser fileChooser = new FileChooser();        
-        fileChooser.setTitle("Choose a Media File");        
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose a Media File");
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("All Media Types", "*.mkv", "*.mp4", "*.mp3", "*.ogg"),
                 new FileChooser.ExtensionFilter("MKV", "*.mkv"),
@@ -45,8 +47,7 @@ public class FXMLDocumentController implements Initializable {
                 new FileChooser.ExtensionFilter("MP3", "*.mp3"),
                 new FileChooser.ExtensionFilter("OGG", "*.ogg")
         );
-        
-        
+
         if (lastDirectory != null) {
             fileChooser.setInitialDirectory(new File(lastDirectory));
         }
@@ -54,48 +55,48 @@ public class FXMLDocumentController implements Initializable {
         File file = fileChooser.showOpenDialog(null);
         lastDirectory = file.getParent();
 
-        if (file != null) {            
+        if (file != null) {
             //MultimediaInfo mi = new MultimediaInfo();                                    
             String ext = FilenameUtils.getExtension(file.getPath());
             /*
-            Encoder e = new Encoder();
+             Encoder e = new Encoder();
             
-            MultimediaInfo mi = e.getInfo(file);
-            AudioInfo ai = mi.getAudio();
-            System.out.println(ai.getDecoder());            
-            String[] s = e.getSupportedDecodingFormats();
-            String[] s1 = e.getSupportedEncodingFormats();
-            String[] s3 = e.getAudioEncoders();
-            String[] s4 = e.getVideoEncoders();
-            String[] s5 = e.getAudioDecoders();
-            */
+             MultimediaInfo mi = e.getInfo(file);
+             AudioInfo ai = mi.getAudio();
+             System.out.println(ai.getDecoder());            
+             String[] s = e.getSupportedDecodingFormats();
+             String[] s1 = e.getSupportedEncodingFormats();
+             String[] s3 = e.getAudioEncoders();
+             String[] s4 = e.getVideoEncoders();
+             String[] s5 = e.getAudioDecoders();
+             */
             if (ext.equals("ogg") || ext.equals("mkv")) {
                 /*
-                EncodingAttributes ea = new EncodingAttributes();
-                ea.setFormat("mp3"); 
+                 EncodingAttributes ea = new EncodingAttributes();
+                 ea.setFormat("mp3"); 
                 
-                AudioAttributes aa = new AudioAttributes();
-                aa.setCodec("pcm_alaw");
-                aa.setVolume(256);
-                ea.setAudioAttributes(aa);
-                ea.setVideoAttributes(null);                
-                e.encode(file,file,ea, new EncoderProgressListener() {
+                 AudioAttributes aa = new AudioAttributes();
+                 aa.setCodec("pcm_alaw");
+                 aa.setVolume(256);
+                 ea.setAudioAttributes(aa);
+                 ea.setVideoAttributes(null);                
+                 e.encode(file,file,ea, new EncoderProgressListener() {
 
-                    @Override
-                    public void sourceInfo(MultimediaInfo mi) {
-                    }
+                 @Override
+                 public void sourceInfo(MultimediaInfo mi) {
+                 }
 
-                    @Override
-                    public void progress(int i) {
-                        System.out.println("" + i);
-                    }
+                 @Override
+                 public void progress(int i) {
+                 System.out.println("" + i);
+                 }
 
-                    @Override
-                    public void message(String string) {
-                    }
-                });
-                //initPlayer(f.toURI().toString());
-                        */
+                 @Override
+                 public void message(String string) {
+                 }
+                 });
+                 //initPlayer(f.toURI().toString());
+                 */
             }
 
             initPlayer(file.toURI().toString());
@@ -124,4 +125,15 @@ public class FXMLDocumentController implements Initializable {
             }
         });
     }
+
+    @FXML
+    public void changeMouseIconToHand() {
+        mediaView.getScene().setCursor(Cursor.HAND);
+    }
+
+    @FXML
+    public void changeMouseIconToPointer() {
+        mediaView.getScene().setCursor(Cursor.DEFAULT);
+    }
+
 }
